7a3f198e3f70c47a870f705125aa8c80d9412016
ckubal <ckubal+gh@gmail.com>
2025-12-01 17:46:34 -0800

    Initial Commit

---
+++ magic eight/magic_eightApp.swift
@@ -0,0 +1,17 @@
+//
+//  magic_eightApp.swift
+//  magic eight
+//
+//  Created by Charlie Kubal on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct magic_eightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
